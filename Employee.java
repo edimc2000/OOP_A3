18d4599ab1c9f55310471e30ee17326108411677
Eddie C. <edimc2000@gmail.com>
2025-11-02 20:00:25 -0600

    drafter parts 1-4

--- Employee.java
+++ Employee.java
@@ -1,5 +1,10 @@
 package OOP_A3;
 
+import java.util.Scanner;
+import static java.lang.System.*;
+
+import java.util.Calendar;
+
 public class Employee {
 
     private String name;
@@ -14,5 +19,50 @@ public class Employee {
         this.birthWeek = birthWeek;
     }
 
-    
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSocialSecurityNumber() {
+        return this.socialSecurityNumber;
+    }
+
+    public int getBirthMonth() {
+        return this.birthMonth;
+    }
+
+    public int getBirthWeek() {
+        return this.birthWeek;
+    }
+
+    // requires more to get entry and load - see requirement 2
+    public static void load() {
+        Scanner userInput = new Scanner(System.in);
+        out.println("type Hourly(1), Salaried(2), Salaried plus Commission(3)");
+        out.println("Enter 1, 2, or 3 ==> ");
+        String employeeNumber = userInput.nextLine();
+        out.println("your entry is : " + employeeNumber);
+
+    }
+
+    @Override // pay check computation missing
+    public String toString() {
+        String display = String.format("%s\t\t: %s\n%s\t: %s\n%s\t\t: $%s",
+                "Employee", this.getName(), "Social Security Number", this.getSocialSecurityNumber(),
+                "Paycheck", "<fix this>");
+        return display;
+    }
+
+
+    // once the pay check variable is created, match this part to auto increment the paycheck by 100 
+    public double getBonus() {
+        // part 4
+        // getting the current month and week to determine eligibility for $100 birthday bonus 
+        Calendar calendar = Calendar.getInstance();
+        int currentWeekOfMonth = calendar.get(Calendar.WEEK_OF_MONTH);
+        int currentMonth = calendar.get(Calendar.MONTH);
+        boolean isEligibleForBonus = (currentMonth == this.birthMonth) && (currentWeekOfMonth == this.birthWeek);
+        return isEligibleForBonus ? 100 : 0;
+    }
+
 }
--- EmployeeDriver.java
+++ EmployeeDriver.java
@@ -1,14 +1,28 @@
-package OOP_A3; 
+package OOP_A3;
 
 import static java.lang.System.*;
 
 public class EmployeeDriver {
     public static void main(String[] args) {
-        
-        Employee emp1 = new Employee("Cardo", "111-222-333", 1, 2); 
 
-        
+        Employee emp1 = new Employee("Cardo", "111-222-333", 10, 2);
+        Employee emp2 = new Employee("dalisay", "111-222-333", 1, 2  );
 
+        // out.println(emp1.getName());
+        // out.println(emp1.getSocialSecurityNumber());
+        // out.println(emp1.getBirthMonth());
+        // out.println(emp1.getBirthWeek());
 
+        // Employee.load();
+        // out.println(emp1.toString());
+
+        out.println(emp1.getName());
+        out.println(emp1.getBonus());
+
+out.println("--------------------------");
+        out.println(emp2.getName());
+        out.println(emp2.getBonus());
     }
+
+
 }
