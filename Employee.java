4893b6b74035bb8e804eb75afbd752c65dbfbc3b
Eddie C. <edimc2000@gmail.com>
2025-11-02 19:19:22 -0600

    initial commit

---
+++ Employee.java
@@ -0,0 +1,18 @@
+package OOP_A3;
+
+public class Employee {
+
+    private String name;
+    private String socialSecurityNumber;
+    private int birthMonth;
+    private int birthWeek;
+
+    Employee(String name, String socialSecurityNumber, int birthMonth, int birthWeek) {
+        this.name = name;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.birthMonth = birthMonth;
+        this.birthWeek = birthWeek;
+    }
+
+    
+}
---
+++ EmployeeDriver.java
@@ -0,0 +1,14 @@
+package OOP_A3; 
+
+import static java.lang.System.*;
+
+public class EmployeeDriver {
+    public static void main(String[] args) {
+        
+        Employee emp1 = new Employee("Cardo", "111-222-333", 1, 2); 
+
+        
+
+
+    }
+}
